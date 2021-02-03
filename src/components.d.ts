efea69520f2154bf1d5d583d91b4638971e895af
Valentin <valentin.g.l@gmail.com>
2021-02-03 23:57:39 +0100

    Side drawer WIP - 1

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,7 @@
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
     interface ScvSideDrawer {
+        "title": string;
     }
 }
 declare global {
@@ -22,6 +23,7 @@ declare global {
 }
 declare namespace LocalJSX {
     interface ScvSideDrawer {
+        "title"?: string;
     }
     interface IntrinsicElements {
         "scv-side-drawer": ScvSideDrawer;
